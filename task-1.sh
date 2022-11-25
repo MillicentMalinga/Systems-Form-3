323b99cc65cdf71d553ef45a2b0297de150f6039
MillicentMalinga <90625689+MillicentMalinga@users.noreply.github.com>
2022-11-25 20:06:11 +0200

    Update task-1.sh

--- task-1.sh
+++ task-1.sh
@@ -1,13 +1,10 @@
 # unzips the zipped folder given as first command line argument into the current directory
 # and then opens the unzipped folder
 
-unzip "$1" -d .;
-echo "Unzipped $1"; 
+
+unzip "data-shell.zip" -d ~/Desktop 
+cd ~/Desktop/data-shell
 echo;
 echo "Showing file types in the unzipped folder:";
 find . -type f -name '*.*' | sed 's|.*\.||' | sort -u
 echo "--------------------------------------------------------"
-ls $(basename "$1" .zip);
-
-unzip "data-shell.zip" -d ~/Desktop 
-ls ~/Desktop/data-shell
