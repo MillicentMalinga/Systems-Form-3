7542dd2a84c2733db0def799d6d85fda720e022e
MillicentMal <millicentmalinga19@gmail.com>
2022-11-25 19:31:27 +0200

    Changing task-1

--- task-1.sh
+++ task-1.sh
@@ -3,4 +3,8 @@
 
 unzip "$1" -d .;
 echo "Unzipped $1"; 
+echo;
+echo "Showing file types in the unzipped folder:";
+find . -type f -name '*.*' | sed 's|.*\.||' | sort -u
+echo "--------------------------------------------------------"
 ls $(basename "$1" .zip);
